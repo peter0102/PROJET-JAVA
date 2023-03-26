6123ba02f82e24e9c45c08b4122b9e191a640b40
Peter <lin.peter@outlook.fr>
2023-03-26 17:55:38 +0200

    copie d'un dossier et de tous ses fichiers

--- src/copy/Copy.java
+++ src/copy/Copy.java
@@ -1,26 +1,34 @@
 package copy;
 import java.io.*;
 public class Copy {
-    public static void main(String args[]) throws FileNotFoundException { 
-            File source = new File("C:\\Users\\LINPa\\Documents\\test\\test.txt");
-            File destination = new File("C:\\Users\\LINPa\\Documents\\test\\test2.txt");
-            try {
-                FileInputStream in= new FileInputStream(source);
-                FileOutputStream out= new FileOutputStream(destination);
-                byte[] buffer= new byte[1024];
-                int length;
-                while ((length = in.read(buffer)) > 0) {
-                    out.write(buffer, 0, length);
+    public static void main(String[] args) throws FileNotFoundException {
+        File folder = new File("C:\\Users\\LINPa\\Documents\\test");
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                File newFolder = new File("C:\\Users\\LINPa\\Documents\\test2");
+                if (!newFolder.exists()) {
+                    newFolder.mkdir();
                 }
-                in.close();
-                out.close();
+                for (File file : folder.listFiles()) {
+                    try {
+                        FileInputStream in = new FileInputStream(file.getPath());
+                        FileOutputStream out = new FileOutputStream(newFolder.getPath() + "\\" + file.getName());
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = in.read(buffer)) > 0) {
+                            out.write(buffer, 0, length);
+                        }
+                        in.close();
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    
+                } 
             }
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        } else {
+            throw new FileNotFoundException("Folder does not exist or is not a directory");
+        }
     }
 }
