84a84b09abe239f7681776f0bca3d3ab770236d4
peter0102 <lin.peter@outlook.fr>
2023-04-19 21:40:51 +0200

    test

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -5,8 +5,12 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("server_hostname", 1234); // create client socket and connect to server
-        System.out.println("Connection established!"); // print message to console
-        socket.close(); // close client socket
+        try {
+            Socket socket = new Socket("localhost", 1234); // create client socket and connect to server
+            System.out.println("Connection established!"); // print message to console
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
--- src/sync/Server.java
+++ src/sync/Server.java
@@ -5,9 +5,13 @@ import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1234); // create server socket
-        Socket clientSocket = serverSocket.accept(); // wait for client to connect
-        System.out.println("Connection established!"); // print message to console
-        serverSocket.close(); // close server socket
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234); // create server socket
+            Socket clientSocket = serverSocket.accept(); // wait for client to connect
+            System.out.println("Connection established!"); // print message to console
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
