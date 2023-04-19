b06a158cefbdda6cd92d0634b23b0e0388f7e0b9
peter0102 <lin.peter@outlook.fr>
2023-04-19 20:29:03 +0200

    test rmi registry

--- src/sync/SyncClient.java
+++ src/sync/SyncClient.java
@@ -7,7 +7,8 @@ import java.io.File;
 
 public class SyncClient {
     public static void main(String[] args) throws Exception {
-        SyncInterface sync = (SyncInterface) Naming.lookup("rmi://localhost/SyncService");
+        Registry registry = LocateRegistry.getRegistry("localhost", 1234);
+        SyncInterface sync = (SyncInterface) registry.lookup("SyncService");
         File sourceFolder = new File("sourceFolder");
         File destinationFolder = new File("destinationFolder");
         sync.syncFolders(sourceFolder, destinationFolder);
--- src/sync/SyncServer.java
+++ src/sync/SyncServer.java
@@ -6,8 +6,14 @@ import java.rmi.registry.Registry;
 
 public class SyncServer {
     public static void main(String[] args) throws Exception {
-        SyncImplementation syncImpl = new SyncImplementation();
-        Naming.rebind("SyncService",syncImpl);
-        System.out.println("Sync service started");
+        try {
+            Registry registry = LocateRegistry.createRegistry(1234);
+            SyncImplementation syncImpl = new SyncImplementation();
+            registry.rebind("SyncService",syncImpl);
+            System.out.println("Sync service started");
+        }
+        catch (Exception e) {
+            System.out.println("Sync service failed: " + e);
+        }
     }
 }
