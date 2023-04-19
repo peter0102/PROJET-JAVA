f9121b622312fe224b659c6869ac37b9b131146c
peter0102 <lin.peter@outlook.fr>
2023-04-19 20:19:35 +0200

    test rmi

--- src/sync/SyncClient.java
+++ src/sync/SyncClient.java
@@ -1,15 +1,16 @@
 package sync;
 
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.io.File;
 
 public class SyncClient {
     public static void main(String[] args) throws Exception {
-        Registry registry = LocateRegistry.getRegistry("serverHostname");
-        SyncInterface sync = (SyncInterface) registry.lookup("SyncService");
+        SyncInterface sync = (SyncInterface) Naming.lookup("rmi://localhost/SyncService");
         File sourceFolder = new File("sourceFolder");
         File destinationFolder = new File("destinationFolder");
         sync.syncFolders(sourceFolder, destinationFolder);
+        System.out.println("Sync done");
     }
 }
