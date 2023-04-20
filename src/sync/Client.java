d97cb16375e8f4639c0c7109e41161f92cb23f97
peter0102 <lin.peter@outlook.fr>
2023-04-21 00:14:51 +0200

    .

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -13,25 +13,27 @@ public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("192.168.1.100", 8000);
         
-        File file = new File("C:\\Users\\Peter\\Documents\\test\\dofus-2022-06-29_19-09-08-Photofiiltre.png");
-        // Get the size of the file
-        long length = file.length();
-        byte[] bytes = new byte[1024];
-        InputStream in = new FileInputStream(file);
-        OutputStream out = socket.getOutputStream();
-        DataOutputStream dataOut = new DataOutputStream(out);
-        String fileName = file.getName();
-        // Send the file name to the server
-        dataOut.writeUTF(fileName);
-        
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
+        File fileDir = new File("C:\\Users\\Peter\\Documents\\test");
+        File[] files = fileDir.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                long length = file.length();
+                byte[] bytes = new byte[1024];
+                InputStream in = new FileInputStream(file);
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dataOut = new DataOutputStream(out);
+                String fileName = file.getName();
+                // Send the file name to the server
+                dataOut.writeUTF(fileName);
+                
+                int count;
+                while ((count = in.read(bytes)) > 0) {
+                    out.write(bytes, 0, count);
+                }
+                in.close();
+            }
         }
 
-        out.close();
-        in.close();
         socket.close();
     }
 }
-
