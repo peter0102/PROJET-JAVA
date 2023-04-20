5e4c31ad55cf5d14939b3604bd72a70d93167dad
peter0102 <lin.peter@outlook.fr>
2023-04-21 00:17:29 +0200

    .

--- src/sync/Server.java
+++ src/sync/Server.java
@@ -13,45 +13,50 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-		while (true) {
-			ServerSocket serverSocket = null;
+        ServerSocket serverSocket = new ServerSocket(8000);
 
-			try {
-				serverSocket = new ServerSocket(8000);
-			} catch (IOException ex) {
-				System.out.println("Can't setup server on this port number. ");
-			}
-	
-			Socket socket = null;
-			InputStream in = null;
-			OutputStream out = null;
-	
-			try {
-				socket = serverSocket.accept();
-			} catch (IOException ex) {
-				System.out.println("Can't accept client connection. ");
-			}
-	
-			try {
-				// Read the file name from the input stream
-				DataInputStream dataIn = new DataInputStream(socket.getInputStream());
-				String fileName = dataIn.readUTF();
-	
-				// Open a file output stream with the received file name
-				out = new FileOutputStream("C:\\Users\\LINPa\\Documents\\" + fileName);
-			} catch (FileNotFoundException ex) {
-				System.out.println("File not found. ");
-			}
-	
-			byte[] bytes = new byte[1024];
-			int count;
-			while ((count = socket.getInputStream().read(bytes)) > 0) {
-				out.write(bytes, 0, count);
-			}
-	
-			out.close();
-			socket.close();
-			serverSocket.close();
-		}
+        while (true) {
+            Socket socket = null;
+            InputStream in = null;
+            OutputStream out = null;
+
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException ex) {
+                System.out.println("Can't accept client connection. ");
+            }
+
+            try {
+                in = socket.getInputStream();
+            } catch (IOException ex) {
+                System.out.println("Can't get socket input stream. ");
+            }
+
+            try {
+                // Read the file name from the input stream
+                DataInputStream dataIn = new DataInputStream(in);
+                String fileName = dataIn.readUTF();
+
+                // Open a file output stream with the received file name
+                out = new FileOutputStream("C:\\Users\\LINPa\\Documents\\" + fileName);
+            } catch (FileNotFoundException ex) {
+                System.out.println("File not found. ");
+            }
+
+            byte[] bytes = new byte[1024];
+
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+            }
+
+            // Close the output stream after each file is written
+            out.close();
+
+            // Close the input stream and socket
+            in.close();
+            socket.close();
+        }
     }
 }
+
