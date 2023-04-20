47a558b40af6402d70e8d4e3d10cfbd6d339cffd
peter0102 <lin.peter@outlook.fr>
2023-04-20 22:52:17 +0200

    test séparé gui

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -2,6 +2,7 @@ package sync;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -11,15 +12,29 @@ public class Client {
         String sourceFolderPath = "C:\\Users\\Peter\\Documents\\test";
         String destinationFolderPath = "C:\\Users\\LinPa\\Documents";
         try {
-            FileInputStream fileInputStream = new FileInputStream(sourceFolderPath);
             Socket socket = new Socket("192.168.1.100", 8000); // create client socket and connect to server
             System.out.println("Connection established!"); // print message to console
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.write(sourceFolderPath.getBytes());
-            dataOutputStream.write(destinationFolderPath.getBytes());
+            dataOutputStream.writeUTF(sourceFolderPath);
+            dataOutputStream.writeUTF(destinationFolderPath);
+            File sourceFolder = new File(sourceFolderPath);
+            File[] files = sourceFolder.listFiles();
+            for (File file : files) {
+                if (file.isFile()) {
+                    dataOutputStream.writeUTF(file.getName());
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fileInputStream.read(buffer)) > 0) {
+                        dataOutputStream.write(buffer, 0, length);
+                    }
+                    fileInputStream.close();
+                    System.out.println("Sent file: " + file.getName());
+                }
+            }
+            dataOutputStream.writeUTF("done");
             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             String response = dataInputStream.readUTF();
-
             System.out.println(response);
 
         }
--- src/sync/Server.java
+++ src/sync/Server.java
@@ -3,53 +3,59 @@ package sync;
 import java.net.*;
 import java.io.*;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) throws IOException {
-        try {
-            ServerSocket serverSocket = new ServerSocket(8000); // create server socket
-            Socket clientSocket = serverSocket.accept(); // wait for client to connect
-            System.out.println("Connection established!"); // print message to console
-            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+        ServerSocket serverSocket = new ServerSocket(8000);
+        System.out.println("Server started and listening on port 8000...");
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("New client connected: " + socket.getInetAddress().getHostAddress());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             String sourceFolderPath = dataInputStream.readUTF();
             String destinationFolderPath = dataInputStream.readUTF();
-            File destDir = new File(destinationFolderPath);
-            if (!destDir.exists()) {
-                destDir.mkdirs();
-            }
-            File sourceDir = new File(sourceFolderPath);
-            copy(sourceDir, destDir,0,0);
+            File sourceFolder = new File(sourceFolderPath);
 
-            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-            dataOutputStream.writeUTF("Folder copied successfully!");
+            copyFolder(sourceFolder, new File(destinationFolderPath));
 
-        }
-        catch (IOException f) {
-            System.out.println("Error: " + f);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF("Copy operation completed successfully!");
+            dataOutputStream.flush();
+
+            socket.close();
         }
     }
-    public static void copy(File source, File destination, int i, int lvl) throws IOException {
-		File[] sourceDir = source.listFiles();
-		if (i == sourceDir.length) {
-			return;
-		}
-		if (sourceDir[i].isFile()) {
-			FileInputStream inputStream = new FileInputStream(sourceDir[i]);
-			FileOutputStream outputStream = new FileOutputStream(new File(destination.getPath() + "\\" + sourceDir[i].getName()));
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = inputStream.read(buffer)) > 0) {
-				outputStream.write(buffer, 0, length);
-			}
-			inputStream.close();
-			outputStream.close();
-		}
-		if (sourceDir[i].isDirectory()) {
-			File newDir = new File(destination.getPath() + "\\" + sourceDir[i].getName());
-			if (!newDir.exists()) {
-				newDir.mkdir();
-			}
-			copy(sourceDir[i], newDir, 0, lvl+1);
-		}
-		copy(source,destination, i + 1, lvl);
-	}
+
+    public static void copyFolder(File source, File destination) throws IOException {
+        if (source.isDirectory()) {
+            if (!destination.exists()) {
+                destination.mkdir();
+            }
+
+            String[] files = source.list();
+            for (String file : files) {
+                File srcFile = new File(source, file);
+                File destFile = new File(destination, file);
+                copyFolder(srcFile, destFile);
+            }
+        } else {
+            FileInputStream inputStream = new FileInputStream(source);
+            FileOutputStream outputStream = new FileOutputStream(destination);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            inputStream.close();
+            outputStream.close();
+        }
+    }
 }
