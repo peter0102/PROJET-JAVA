981af90376a7ec9b388e6149a681a3a3d9494963
peter0102 <lin.peter@outlook.fr>
2023-04-21 01:13:46 +0200

    1

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -1,47 +1,45 @@
 package sync;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Client {
+    private static Socket socket;
+    private static BufferedOutputStream bos;
+    private static DataOutputStream dos;
+    private static String rootDir;
+
     public static void main(String[] args) throws IOException {
-        String directory = "C:\\Users\\Peter\\Documents\\test";
-        
-        File[] files = new File(directory).listFiles();
-        
-        Socket socket = new Socket("192.168.1.100", 8000);
-        
-        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
-        DataOutputStream dos = new DataOutputStream(bos);
-        
-        dos.writeInt(files.length);
-        
-        for(File file : files)
-        {
-            long length = file.length();
-            dos.writeLong(length);
-        
-            String name = file.getName();
-            dos.writeUTF(name);
-        
-            FileInputStream fis = new FileInputStream(file);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-        
-            int theByte = 0;
-            while((theByte = bis.read()) != -1) bos.write(theByte);
-        
-            bis.close();
-        }
-        
+        rootDir = "C:\\Users\\Peter\\Documents\\test";
+        socket = new Socket("192.168.1.100", 8000);
+        bos = new BufferedOutputStream(socket.getOutputStream());
+        dos = new DataOutputStream(bos);
+
+        sendFiles(new File(rootDir));
+
         dos.close();
     }
-}
 
+    public static void sendFiles(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            dos.writeUTF(file.getAbsolutePath());
+            dos.writeInt(files.length);
+            for (File f : files) {
+                sendFiles(f);
+            }
+        } else {
+            dos.writeUTF(file.getAbsolutePath().replace(rootDir, ""));
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            byte[] buffer = new byte[8192];
+            int count;
+            while ((count = bis.read(buffer)) > 0) {
+                bos.write(buffer, 0, count);
+            }
+
+            bis.close();
+        }
+    }
+}
--- src/sync/Server.java
+++ src/sync/Server.java
@@ -1,10 +1,8 @@
 package sync;
 
-import java.net.*;
-import java.io.*;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,36 +11,51 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        while (true) {
-			String dirPath ="C:\\Users\\LINPa\\Documents\\";
+        String dirPath = "C:\\Users\\LINPa\\Documents\\";
 
-			ServerSocket serverSocket = new ServerSocket(8000);
-			Socket socket = serverSocket.accept();
-			
-			BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
-			DataInputStream dis = new DataInputStream(bis);
-			
-			int filesCount = dis.readInt();
-			File[] files = new File[filesCount];
-			
-			for(int i = 0; i < filesCount; i++)
-			{
-				long fileLength = dis.readLong();
-				String fileName = dis.readUTF();
-			
-				files[i] = new File(dirPath + "\\" + fileName);
-			
-				FileOutputStream fos = new FileOutputStream(files[i]);
-				BufferedOutputStream bos = new BufferedOutputStream(fos);
-			
-				for(int j = 0; j < fileLength; j++) bos.write(bis.read());
-			
-				bos.close();
-			}
-			
-			dis.close();
-			socket.close();
-            serverSocket.close();
+        ServerSocket serverSocket = new ServerSocket(8000);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+            DataInputStream dis = new DataInputStream(bis);
+
+            int filesCount = dis.readInt();
+            File[] files = new File[filesCount];
+
+            for (int i = 0; i < filesCount; i++) {
+                long fileLength = dis.readLong();
+                String fileName = dis.readUTF();
+
+                File file = new File(dirPath + "\\" + fileName);
+
+                // Create any required subdirectories
+                File parent = file.getParentFile();
+                if (parent != null && !parent.exists()) {
+                    parent.mkdirs();
+                }
+
+                // Check if the file already exists
+                if (file.exists()) {
+                    // Handle the file already exists case as needed
+                    // For example, you could skip the file or overwrite it
+                    System.out.println("File already exists: " + file.getAbsolutePath());
+                } else {
+                    // Create the file and write the contents
+                    FileOutputStream fos = new FileOutputStream(file);
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+                    for (int j = 0; j < fileLength; j++) {
+                        bos.write(bis.read());
+                    }
+
+                    bos.close();
+                    System.out.println("File created: " + file.getAbsolutePath());
+                }
+            }
+
+            dis.close();
+            socket.close();
         }
     }
 }
