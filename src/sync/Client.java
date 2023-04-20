e8af22ef76c7b50c6a7267ea5315ae606275e703
peter0102 <lin.peter@outlook.fr>
2023-04-20 23:43:30 +0200

    3

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -5,35 +5,31 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String sourceFolderPath = "C:\\Users\\Peter\\Documents\\test";
-        String destinationFolderPath = "C:\\Users\\LinPa\\Documents";
-        File sourceFile = new File("C:\\Users\\Peter\\Documents\\test\\test1.txt");
-        try {
-            Socket socket = new Socket("192.168.1.100", 8000); // create client socket and connect to server
-            System.out.println("Connection established!"); // print message to console
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF(sourceFolderPath);
-            dataOutputStream.writeUTF(destinationFolderPath);
-            FileInputStream fileInputStream = new FileInputStream(sourceFile);
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                dataOutputStream.write(buffer, 0, bytesRead);
-            }
-            dataOutputStream.writeUTF("done");
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            String response = dataInputStream.readUTF();
-            System.out.println(response);
-            fileInputStream.close();
-            dataOutputStream.close();
-            socket.close();
+        Socket socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket("192.168.1.100", 8000);
+        
+        File file = new File("C:\\Users\\Peter\\Documents\\test\\test1.txt");
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+        
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
         }
-        catch (IOException f) {
-            System.out.println("Error: " + f);
-        }
+
+        out.close();
+        in.close();
+        socket.close();
     }
 }
--- src/sync/Server.java
+++ src/sync/Server.java
@@ -12,67 +12,47 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8000);
-        System.out.println("Server started and listening on port 8000...");
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = null;
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("New client connected: " + socket.getInetAddress().getHostAddress());
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-			while (true) {
-				String fileName = dataInputStream.readUTF();
-	
-				// Break the loop if the client signals that all files have been sent
-				if (fileName.equals("done")) {
-					System.out.println("All files received from client");
-					break;
-				}
-	
-				// Create output stream to write file to destination folder
-				FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\LINPa\\Documents\\" + fileName);
-	
-				// Read file data from client and write it to output stream
-				byte[] buffer = new byte[1024];
-				int length;
-				while ((length = dataInputStream.read(buffer)) > 0) {
-					fileOutputStream.write(buffer, 0, length);
-				}
-				fileOutputStream.close();
-				System.out.println("Received file: " + fileName);
-			}
-	
-			// Send response to client
-			dataInputStream.close();
-			socket.close();
-			serverSocket.close();
-			System.out.println("Transfer completed successfully");
+		try {
+			serverSocket = new ServerSocket(8000);
+		} catch (IOException ex) {
+			System.out.println("Can't setup server on this port number. ");
 		}
-		
-    }
 
-    public static void copyFolder(File source, File destination) throws IOException {
-        if (source.isDirectory()) {
-            if (!destination.exists()) {
-                destination.mkdir();
-            }
+		Socket socket = null;
+		InputStream in = null;
+		OutputStream out = null;
 
-            String[] files = source.list();
-            for (String file : files) {
-                File srcFile = new File(source, file);
-                File destFile = new File(destination, file);
-                copyFolder(srcFile, destFile);
-            }
-        } else {
-            FileInputStream inputStream = new FileInputStream(source);
-            FileOutputStream outputStream = new FileOutputStream(destination);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
-            }
-            inputStream.close();
-            outputStream.close();
-        }
-    }
+		try {
+			socket = serverSocket.accept();
+		} catch (IOException ex) {
+			System.out.println("Can't accept client connection. ");
+		}
+
+		try {
+			in = socket.getInputStream();
+		} catch (IOException ex) {
+			System.out.println("Can't get socket input stream. ");
+		}
+
+		try {
+			out = new FileOutputStream("C:\\Users\\LINPa\\Documents\\test1.txt");
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found. ");
+		}
+
+		byte[] bytes = new byte[1024];
+
+		int count;
+		while ((count = in.read(bytes)) > 0) {
+			out.write(bytes, 0, count);
+		}
+
+		out.close();
+		in.close();
+		socket.close();
+		serverSocket.close();
+	}
 }
