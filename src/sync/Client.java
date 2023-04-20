39f72f12067370d417e8f0cd8282e286a2d7f867
peter0102 <lin.peter@outlook.fr>
2023-04-20 22:41:57 +0200

    test 2

--- src/sync/Client.java
+++ src/sync/Client.java
@@ -1,16 +1,30 @@
 package sync;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        String sourceFolderPath = "C:\\Users\\Peter\\Documents\\test";
+        String destinationFolderPath = "C:\\Users\\LinPa\\Documents";
         try {
-            Socket socket = new Socket("localhost", 1234); // create client socket and connect to server
+            FileInputStream fileInputStream = new FileInputStream(sourceFolderPath);
+            Socket socket = new Socket("192.168.1.100", 8000); // create client socket and connect to server
             System.out.println("Connection established!"); // print message to console
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.write(sourceFolderPath.getBytes());
+            dataOutputStream.write(destinationFolderPath.getBytes());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String response = dataInputStream.readUTF();
+
+            System.out.println(response);
+
         }
-        catch (IOException e) {
-            System.out.println("Error: " + e);
+        catch (IOException f) {
+            System.out.println("Error: " + f);
         }
     }
 }
--- src/sync/Server.java
+++ src/sync/Server.java
@@ -6,12 +6,50 @@ import java.io.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         try {
-            ServerSocket serverSocket = new ServerSocket(1234); // create server socket
+            ServerSocket serverSocket = new ServerSocket(8000); // create server socket
             Socket clientSocket = serverSocket.accept(); // wait for client to connect
             System.out.println("Connection established!"); // print message to console
+            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+            String sourceFolderPath = dataInputStream.readUTF();
+            String destinationFolderPath = dataInputStream.readUTF();
+            File destDir = new File(destinationFolderPath);
+            if (!destDir.exists()) {
+                destDir.mkdirs();
+            }
+            File sourceDir = new File(sourceFolderPath);
+            copy(sourceDir, destDir,0,0);
+
+            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+            dataOutputStream.writeUTF("Folder copied successfully!");
+
         }
-        catch (IOException e) {
-            System.out.println("Error: " + e);
+        catch (IOException f) {
+            System.out.println("Error: " + f);
         }
     }
+    public static void copy(File source, File destination, int i, int lvl) throws IOException {
+		File[] sourceDir = source.listFiles();
+		if (i == sourceDir.length) {
+			return;
+		}
+		if (sourceDir[i].isFile()) {
+			FileInputStream inputStream = new FileInputStream(sourceDir[i]);
+			FileOutputStream outputStream = new FileOutputStream(new File(destination.getPath() + "\\" + sourceDir[i].getName()));
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inputStream.read(buffer)) > 0) {
+				outputStream.write(buffer, 0, length);
+			}
+			inputStream.close();
+			outputStream.close();
+		}
+		if (sourceDir[i].isDirectory()) {
+			File newDir = new File(destination.getPath() + "\\" + sourceDir[i].getName());
+			if (!newDir.exists()) {
+				newDir.mkdir();
+			}
+			copy(sourceDir[i], newDir, 0, lvl+1);
+		}
+		copy(source,destination, i + 1, lvl);
+	}
 }
