cf598a722e4566829fd4eba0248245e245a8ebdd
peter0102 <lin.peter@outlook.fr>
2023-04-21 13:05:34 +0200

    test

--- src/sync/Server.java
+++ src/sync/Server.java
@@ -14,41 +14,37 @@ import java.net.Socket;
 public class Server {
     
     private static void receiveFiles(Socket socket, String dirPath) throws IOException {
-        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
-        DataInputStream dis = new DataInputStream(bis);
+        try (BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+             DataInputStream dis = new DataInputStream(bis)) {
 
-        int filesCount = dis.readInt();
-        File[] files = new File[filesCount];
+            int filesCount = dis.readInt();
+            File[] files = new File[filesCount];
 
-        for (int i = 0; i < filesCount; i++) {
-            long fileLength = dis.readLong();
-            String fileName = dis.readUTF();
+            for (int i = 0; i < filesCount; i++) {
+                long fileLength = dis.readLong();
+                String fileName = dis.readUTF();
 
-            files[i] = new File(dirPath + "\\" + fileName);
+                files[i] = new File(dirPath + "\\" + fileName);
 
-            FileOutputStream fos = new FileOutputStream(files[i]);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
+                try (FileOutputStream fos = new FileOutputStream(files[i]);
+                     BufferedOutputStream bos = new BufferedOutputStream(fos)) {
 
-            for (int j = 0; j < fileLength; j++) {
-                bos.write(bis.read());
+                    for (int j = 0; j < fileLength; j++) {
+                        bos.write(bis.read());
+                    }
+                }
             }
-
-            bos.close();
         }
-
-        dis.close();
     }
 
     public static void main(String[] args) throws IOException {
         String dirPath = "C:\\Users\\LINPa\\Documents\\";
-
-        ServerSocket serverSocket = new ServerSocket(8000);
-
-        while (true) {
-            Socket socket = serverSocket.accept();
-            receiveFiles(socket, dirPath);
-            socket.close();
-			serverSocket.close();
-        }   
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    receiveFiles(socket, dirPath);
+                }
+            }
+        }
     }
 }
